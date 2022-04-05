34ab3d14f622a550f6d51bbb2aae4cdcd23db224
Snazzah <suggesttosnazzy@gmail.com>
2022-04-04 19:24:26 -0500

    feat: finish home banner

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -1,13 +1,28 @@
 <template>
   <main
     class="home"
+    :class="`home${data.giarc ? ' giarc' : ''}`"
     role="main"
     v-show-on-load
   >
     <div class="outer-banner">
       <div class="banner">
         <img src="/icon-512x512.png" no-zoom draggable="false">
-        <div class="banner-text"><!-- TODO banner text and buttons --></div>
+        <div class="banner-main">
+          <div class="banner-text">
+            <h1>{{ data.title || $title }}</h1>
+            <p>{{ data.tagline }}</p>
+          </div>
+          <div class="banner-buttons">
+            <NavLink
+              class="action-button"
+              v-for="abtn in data.actionButtons"
+              :key="abtn.text"
+              :class="abtn.primary ? 'primary': ''"
+              :item="abtn"
+            />
+          </div>
+        </div>
       </div>
     </div>
 
@@ -18,9 +33,11 @@
 <script>
 import Animate from '@theme/animate_scroll.js'
 import ShowOnLoad from '@theme/show_on_load.js'
+import NavLink from '@theme/components/NavLink.vue'
 
 export default {
   name: 'Home',
+  components: { NavLink },
   directives: {
     animate: Animate(),
     showOnLoad: ShowOnLoad()
@@ -34,8 +51,6 @@ export default {
 </script>
 
 <style lang="stylus">
-hero-collapse-point = 1000px
-
 .theme-default-content.custom
   .header-anchor
     display none
@@ -56,19 +71,61 @@ hero-collapse-point = 1000px
     display flex
     align-items center
     justify-content center
-    & > *
-      filter drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.25))
-    @media (max-width: $MQMobile)
-      flex-direction column
+    @media (max-width: $MQNarrow)
       height unset
     .banner
       display flex
       align-items center
       max-width 1200px
       margin 0 auto
+      padding 20px 50px
+      gap 50px
+      @media (max-width: $MQNarrow)
+        flex-direction column
+        gap 5px
       img
         width 300px
         height 300px
+        filter drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.25))
+        @media (max-width: $homePageWidth)
+          width 200px
+          height 200px
+      .banner-main
+        display flex
+        flex-direction column
+        justify-content center
+        gap 20px
+        @media (max-width: $MQNarrow)
+          gap 10px
+        .banner-text
+          display flex
+          flex-direction column
+          filter drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.5))
+          color #FFFFFF
+          h1
+            font-weight 500
+            font-size 64px
+            margin 0
+            @media (max-width: $MQNarrow)
+              text-align center
+          p
+            font-size 24px
+            margin 0
+        .banner-buttons
+          display flex
+          gap 10px
+          @media (max-width: $MQNarrow)
+            justify-content center
+          .action-button
+            padding 10px 15px
+            filter drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.25))
+            color #FFFFFF
+            svg
+              display none
+            &.primary
+              background #007171
+              border-radius 5px
+              filter none
   .theme-default-content
     max-width 1200px
     margin 0 auto
@@ -77,90 +134,9 @@ hero-collapse-point = 1000px
       padding 2rem
     @media (max-width: $MQMobileNarrow)
       padding 1.5rem
-  .features
-    display flex
-    flex-direction column
-    justify-content center
-    align-items center
-    padding 20px 0px
-    width 100%
-    & > * + *
-      margin-top 10px
-    .feature
-      display flex
-      flex-direction row
-      justify-content center
-      align-items center
-      padding 10px
-      width 100%
-      @media (max-width: $MQMobile)
-        flex-direction column
-      &.gilded
-        border 3px solid rgba(255, 184, 0, 0.5)
-        box-sizing border-box
-        border-radius 5px
-        display none
-      img
-        -webkit-user-drag none
-        object-fit cover
-        margin-right 40px
-        width 150px
-        height 150px
-        border-radius 15px
-        filter drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.25))
-        @media (max-width: $MQMobile)
-          margin-right 0
-          margin-bottom 10px
-      .feature-text
-        display flex
-        flex-direction column
-        align-items flex-start
-        padding 0px
-        width 100%
-        h2
-          font-style normal
-          font-weight 500
-          font-size 36px
-          line-height 42px
-          font-feature-settings 'liga' off
-          color #f2f2f2
-          border none
-          margin 0
-          @media (max-width: $MQMobile)
-            font-size 20px
-          .header-anchor
-            display none
-          strong
-            font-weight 900
-            color #fff
-        p
-          font-style normal
-          font-weight normal
-          font-size 18px
-          line-height 21px
-          color #ddd
-          margin 0 0 10px 0
-          @media (max-width: $MQMobile)
-            font-size 16px
-@media (max-width: $MQNarrow)
-  .home
-    .features .feature
-      h2
-        font-size 1.2rem
-      p
-        font-size 0.8rem
-@media (max-width: $MQMobile)
-  .home
-    .features
-      flex-direction column
-      width unset
-      .feature
-        max-width 100%
-        for i in (feature-delay-range)
-          &:nth-child({i})
-            animation-delay 0s
-        h2
-          font-size 1.5rem
-        p
-          font-size 1rem
+
+.home.giarc .outer-banner
+  background-color #A02B11
+  .banner .banner-main .banner-buttons .action-button.primary
+    background #711C08
 </style>
