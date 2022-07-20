5a41cfd0bb92195f717a0895d9a0ef59323fdc95
Snazzah <me@snazzah.com>
2022-07-19 20:15:34 -0500

    feat: update discord message to include sub commands

--- docs/.vuepress/components/CraigAutorecordMessage.vue
+++ docs/.vuepress/components/CraigAutorecordMessage.vue
@@ -2,11 +2,11 @@
   <ClientOnly>
     <discord-messages>
       <discord-message :profile="bot || 'craig'" ephemeral>
-        <discord-command slot="reply" profile="snazzah" command="/autorecord" />
+        <discord-command slot="reply" profile="snazzah" command="/autorecord on" />
         Auto-recording on <discord-mention type="voice">Live</discord-mention> has been activated. Please make sure you can receive DMs from me.
       </discord-message>
       <discord-message v-if="view" :profile="bot || 'craig'" ephemeral>
-        <discord-command slot="reply" profile="snazzah" command="/autorecord" />
+        <discord-command slot="reply" profile="snazzah" command="/autorecord view" />
         <discord-embed slot="embeds" embed-title="Auto-recorded Channels">
           <discord-embed-description slot="description"><discord-mention type="voice">Live</discord-mention> by <discord-mention>Snazzah</discord-mention> (posting to <discord-mention type="channel">live-chat</discord-mention>)</discord-embed-description>
         </discord-embed>
